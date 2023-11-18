6522261a0bf956d7f800f7b08e592b544214804c
Saiful Ajom Khan <sajomkhan@gmail.com>
2023-11-19 01:11:54 +0600

    update searce.js item

--- app/components/dashboard/search.jsx
+++ app/components/dashboard/search.jsx
@@ -1,23 +1,26 @@
 "use client";
+// have to install--------- npm i use-debounce@9.0.4
+// and use as like that -------useDebouncedCallback((e)=>{},300)
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { MdSearch } from "react-icons/md";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (e) => {
+  const handleSearch = useDebouncedCallback((e) => {
     const params = new URLSearchParams(searchParams);
 
     if (e.target.value) {
-      params.set("q", e.target.value);
+      e.target.value.length > 1 && params.set("q", e.target.value);
     } else {
       params.delete("q");
     }
     replace(`${pathname}?${params}`);
-  };
+  }, 400);
 
   return (
     <div className="flex gap-1 items-center w-60 bg-[var(--bgHover)] rounded-lg px-3 py-2">
